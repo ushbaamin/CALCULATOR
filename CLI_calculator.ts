454d15ffba8cf112812b161ff1ed1eb32ba02db7
Ushba <139709104+ushbaamin@users.noreply.github.com>
2023-10-16 13:31:33 -0700

    Add files via upload

---
+++ CLI_calculator.ts
@@ -0,0 +1,43 @@
+#! /usr/bin/env node
+import chalk from "chalk";
+import inquirer from "inquirer";
+
+const operators = ["+","-","/","*"];
+const calculate= (firstNumber:number,secondNumber:number,operator:string)  => {
+
+    switch (operator){
+        case "+":
+            return firstNumber+secondNumber;
+        case "-":
+            return firstNumber-secondNumber;
+        case "/":
+            return firstNumber/secondNumber;
+        case "*":
+            return firstNumber*secondNumber;
+        default:
+            throw new Error("invalid operator")
+    } 
+};
+const calculator= async() => {
+    const { firstNumber } = await inquirer.prompt([{
+        type:"number",
+        name:"firstNumber",
+        message:"Enter first number please",
+    },
+]);const { secondNumber } = await inquirer.prompt([{
+        type:"number",
+        name:"secondNumber",
+        message:"enter second number",
+    },
+]);const { operator } = await inquirer.prompt([{
+        name:"operator",
+        type:"list",
+        choices:operators,
+        message:"Choose the operator which you want to perfrom.",
+    },
+]);
+const result = calculate(firstNumber, secondNumber, operator );
+console.log(result)
+};
+
+calculator();
